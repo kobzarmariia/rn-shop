84c3ad44491384bcadbb614d14415c30f20139ab
kobzarmariia <mariia.kobzar@gmail.com>
2019-11-13 15:45:12 +0200

    [+] login

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -4,9 +4,10 @@ export const ADD_ORDER = 'ADD_ORDER';
 export const SET_ORDERS = 'SET_ORDERS';
 
 export const fetchOrders = () => {
-	return async dispatch => {
+	return async (dispatch, getState) => {
+		const userId = getState().auth.userId;
 		try {
-			const response = await fetch('https://rn-shop-olx.firebaseio.com/orders/u1.json');
+			const response = await fetch(`https://rn-shop-olx.firebaseio.com/orders/${userId}.json`);
 
 			if (!response.ok) {
 				throw new Error('Something went wrong!');
@@ -33,19 +34,24 @@ export const fetchOrders = () => {
 };
 
 export const addOrder = (cartItems, totalAmount) => {
-	return async dispatch => {
+	return async (dispatch, getState) => {
+		const token = getState().auth.token;
+		const userId = getState().auth.userId;
 		const date = new Date();
-		const response = await fetch('https://rn-shop-olx.firebaseio.com/orders/u1.json', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				cartItems,
-				totalAmount,
-				date: date.toISOString(),
-			}),
-		});
+		const response = await fetch(
+			`https://rn-shop-olx.firebaseio.com/orders/${userId}.json?auth=${token}`,
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					cartItems,
+					totalAmount,
+					date: date.toISOString(),
+				}),
+			}
+		);
 
 		if (!response.ok) {
 			throw new Error('Something went wrong!');
--- store/actions/products.js
+++ store/actions/products.js
@@ -6,7 +6,8 @@ export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
 export const SET_PRODUCTS = 'SET_PRODUCTS';
 
 export const fetchProducts = () => {
-	return async dispatch => {
+	return async (dispatch, getState) => {
+		const userId = getState().auth.userId;
 		try {
 			const response = await fetch('https://rn-shop-olx.firebaseio.com/products.json');
 
@@ -21,7 +22,7 @@ export const fetchProducts = () => {
 				loadedProducts.push(
 					new Product(
 						key,
-						'u1',
+						resData[key].ownerId,
 						resData[key].title,
 						resData[key].imageUrl,
 						resData[key].description,
@@ -30,7 +31,11 @@ export const fetchProducts = () => {
 				);
 			}
 
-			dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+			dispatch({
+				type: SET_PRODUCTS,
+				products: loadedProducts,
+				userProducts: loadedProducts.filter(prod => prod.ownerId === userId),
+			});
 		} catch (err) {
 			throw err;
 		}
@@ -38,10 +43,14 @@ export const fetchProducts = () => {
 };
 
 export const deleteProduct = productId => {
-	return async dispatch => {
-		const response = await fetch(`https://rn-shop-olx.firebaseio.com/products/${productId}.json`, {
-			method: 'DELETE',
-		});
+	return async (dispatch, getState) => {
+		const token = getState().auth.token;
+		const response = await fetch(
+			`https://rn-shop-olx.firebaseio.com/products/${productId}.json?auth=${token}`,
+			{
+				method: 'DELETE',
+			}
+		);
 
 		if (!response.ok) {
 			throw new Error('Something went wrong!');
@@ -52,8 +61,11 @@ export const deleteProduct = productId => {
 };
 
 export const createProduct = (title, description, imageUrl, price) => {
-	return async dispatch => {
-		const response = await fetch('https://rn-shop-olx.firebaseio.com/products.json', {
+	return async (dispatch, getState) => {
+		const token = getState().auth.token;
+		const userId = getState().auth.userId;
+
+		const response = await fetch(`https://rn-shop-olx.firebaseio.com/products.json?auth=${token}`, {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
@@ -63,6 +75,7 @@ export const createProduct = (title, description, imageUrl, price) => {
 				description,
 				imageUrl,
 				price,
+				ownerId: userId,
 			}),
 		});
 
@@ -76,24 +89,29 @@ export const createProduct = (title, description, imageUrl, price) => {
 				description,
 				imageUrl,
 				price,
+				ownerId: userId,
 			},
 		});
 	};
 };
 
 export const updateProduct = (id, title, description, imageUrl) => {
-	return async dispatch => {
-		const response = await fetch(`https://rn-shop-olx.firebaseio.com/products/${id}.json`, {
-			method: 'PATCH',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				title,
-				description,
-				imageUrl,
-			}),
-		});
+	return async (dispatch, getState) => {
+		const token = getState().auth.token;
+		const response = await fetch(
+			`https://rn-shop-olx.firebaseio.com/products/${id}.json?auth=${token}`,
+			{
+				method: 'PATCH',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					title,
+					description,
+					imageUrl,
+				}),
+			}
+		);
 
 		if (!response.ok) {
 			throw new Error('Something went wrong!');
